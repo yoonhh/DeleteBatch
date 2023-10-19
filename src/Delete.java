5c7688d547ef421e1c5f6093275adcc534880e1f
yoonhh <hdfull9820@gmail.com>
2023-10-19 16:08:21 +0900

    log4j 수정

--- src/Delete.java
+++ src/Delete.java
@@ -5,10 +5,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import com.windfire.apis.asysConnectData;
 import com.windfire.apis.asys.asysUsrElement;
 
 public class Delete {
+	private static final Logger logger = Logger.getLogger(Delete.class.getName());
 	private asysConnectData con = null;
 	private String Hostname; // 설치예정인 서버 또는 설치된 ‘IP’ 또는 ‘도메인’ 정보
 	private int Port; // XTORM 엔진 내부 통신을 위한 TCP/IP 포트
@@ -20,9 +24,16 @@ public class Delete {
 	public long start = System.currentTimeMillis();
 	public long end = System.currentTimeMillis();
 
-	public Delete() {
+	Properties prop = new Properties();// 키값을 불러오기 위한 객체 생성
+	
+	public Delete() throws IOException {
 
-		Properties prop = new Properties();// 키값을 불러오기 위한 객체 생성
+		FileInputStream logFile = new FileInputStream("log4j.properties");
+		
+		Properties logprop = new Properties();
+				
+		logprop.load(logFile);
+		PropertyConfigurator.configure(logprop);
 
 		try {
 			prop.load(new FileInputStream("conf/info.properties"));
@@ -35,9 +46,12 @@ public class Delete {
 			ID = prop.getProperty("ID");
 			Password = prop.getProperty("Password");
 		} catch (Exception e) {
+			
 		}
-
 		con = new asysConnectData(Hostname, Port, Description, ID, Password);
+	
+		
+		
 	}
 
 	public void deleteBatch() throws IOException {
@@ -55,27 +69,25 @@ public class Delete {
 		int fail = 0;
 
 		while ((elementId = br.readLine()) != null) {
-			System.out.println(elementId);
 			uePage1.m_elementId = "XTORM_MAIN::" + elementId + "::IMAGE";
-			int ret = uePage1.delete();
-			System.out.println(uePage1.m_elementId);
-			System.out.println("delete : " + start / 1000.0 + "초");
+			int ret = uePage1.delete();	
+			logger.info("delete : " + start / 1000.0 + "초");
 			if (ret != 0) {
-				System.out.println("Error, failed to delete, " + uePage1.getLastError());
+				logger.error("Error, failed to delete, " + uePage1.getLastError());
 				fail++;
 
 			} else {
-				System.out.println("Success, delete is done, " + uePage1.m_elementId);
-				System.out.println("delete 완료 : " + (end - start) / 1000.0 + "초");
+				logger.info("Success, delete is done, " + uePage1.m_elementId);
+				logger.info("delete 완료 : " + (end - start) / 1000.0 + "초");
 				success++;
 			}
 			all++;
 		}
 
 		br.close();
-		System.out.println("총건수 : " + all);
-		System.out.println("삭제건수 : " + success);
-		System.out.println("에러건수 : " + fail);
+		logger.info("총건수 : " + all);
+		logger.info("삭제건수 : " + success);
+		logger.info("에러건수 : " + fail);
 
 	}
 
